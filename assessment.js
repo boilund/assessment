497289774075dc4d6eb28e033f21d9a87f588d38
boilund <nanahaan@gmail.com>
2017-01-01 21:44:52 +0100

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -20,7 +20,7 @@
         if (userName.length === 0) { // 名前が空の時は処理を終了する
             return;
         }
-        
+
         // 診断結果表示エリアの作成
         removeAllChildren(resultDivided);
         const header = document.createElement('h3');
@@ -41,8 +41,8 @@
         anchor.className = 'twitter-hashtag-button';
         anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
         tweetDivided.appendChild(anchor);
-
-        twttr.widgets.load();
+        
+                twttr.widgets.load();
     };
 
     userNameInput.onkeydown = (event) => {
@@ -50,8 +50,8 @@
             assessmentButton.onclick();
         }
     };
-
-    const answers = [
+    
+        const answers = [
         '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
         '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
         '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
@@ -75,14 +75,14 @@
     * @param {string} userName ユーザーの名前
     * @return {string} 診断結果
     */
-       function assessment(userName) {
+    function assessment(userName) {
         // 全文字のコード番号を取得してそれを足し合わせる
         let sumOfcharCode = 0;
         for (let i = 0; i < userName.length; i++) {
             sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
         }
-
-        // 文字のコード番号の合計を回答の数で割って添字の数値を求める
+        
+             // 文字のコード番号の合計を回答の数で割って添字の数値を求める
         const index = sumOfcharCode % answers.length;
         let result = answers[index];
 
@@ -95,7 +95,7 @@
         assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
         '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
     );
-        console.assert(
+    console.assert(
         assessment('太郎') === assessment('太郎'),
         '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
     );
